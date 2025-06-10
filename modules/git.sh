da245bc1abae8dd3b539977df1d6fa42f2d3cac6
Dwayn Matthies <dwayn@livekit.io>
2025-06-10 15:25:48 -0500

    Add unaliasing of gitrb and gitrbp before defining as functions for clean upgrade

--- modules/git.sh
+++ modules/git.sh
@@ -1,4 +1,5 @@
 # Git Things
+unalias gitrb gitrbp 2>/dev/null
 gitrb() {
     local default_branch=$(git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | sed 's@^refs/remotes/origin/@@' || echo "master")
     git pull --rebase origin "$default_branch"
